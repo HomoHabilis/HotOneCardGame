cb6f8918225ba366b28bffee0233859fc11c66cd
HomoHabilis <jeremiech@gmail.com>
2025-05-22 23:51:58 +0200

    new questions

--- card-data.js
+++ card-data.js
@@ -154,6 +154,114 @@ const cardData = [
       it: "Racconta il momento più imbarazzante vissuto con uno di noi",
       fr: "Raconte le moment le plus embarrassant vécu avec l’un d’entre nous."
     }
+  },
+  {
+    id: 18,
+    category: "question",
+    text: {
+      en: "What was your worst date (before meeting the bride)?",
+      it: "Qual è stato il tuo peggior appuntamento (con quelle prima della sposa)?",
+      fr: "Quel a été ton pire rendez-vous (avant de rencontrer la mariée) ?"
+    }
+  },
+  {
+    id: 19,
+    category: "question",
+    text: {
+      en: "Who would be the worst person to crash your wedding?",
+      it: "Quale sarebbe la persona peggiore che potrebbe introdursi al tuo matrimonio?",
+      fr: "Quelle serait la pire personne qui pourrait s’incruster à ton mariage ?"
+    }
+  },
+  {
+    id: 20,
+    category: "question",
+    text: {
+      en: "Tell us a moment when you realized Jaleh was the one (if there is one).",
+      it: "Raccontacci (se c'è), un momento di vita fra quelli quando hai capito che Jaleh era quella giusta.",
+      fr: "Raconte-nous un moment où tu as compris que Jaleh était la bonne (s’il y en a un)."
+    }
+  },
+  {
+    id: 21,
+    category: "dare",
+    text: {
+      en: "Act out your last argument without speaking. (If you say you never argue, you get triple sauce.)",
+      it: "Mima la tua ultima litigata senza parlare. (se ci dici che non litigate mai, ti prende tripla salsa).",
+      fr: "Mime ta dernière dispute sans parler. (Si tu dis que vous ne vous disputez jamais, tu prends triple sauce.)"
+    }
+  },
+  {
+    id: 22,
+    category: "question",
+    text: {
+      en: "What’s the craziest thing Jaleh made you do for the wedding prep?",
+      it: "Quale la cosa piu assurda che Jaleh ti ha obbligato a fare per preparare questo matrimonio?",
+      fr: "Quelle est la chose la plus folle que Jaleh t’a obligée à faire pour préparer ce mariage ?"
+    }
+  },
+  {
+    id: 23,
+    category: "question",
+    text: {
+      en: "What did you think the first time you saw her?",
+      it: "Cosa hai pensato quando l’hai vista la prima volta?",
+      fr: "Qu’as-tu pensé la première fois que tu l’as vue ?"
+    }
+  },
+  {
+    id: 24,
+    category: "question",
+    text: {
+      en: "What is the one thing you can’t go on vacation without?",
+      it: "Cosa non puoi fare a meno di portare in vacanza?",
+      fr: "Quel est l’objet que tu ne peux absolument pas oublier en vacances ?"
+    }
+  },
+  {
+    id: 25,
+    category: "punishment",
+    text: {
+      en: "Imitate someone in the group until they guess who it is.",
+      it: "Imita uno dei presenti finché non indovinano chi è",
+      fr: "Imite quelqu’un du groupe jusqu’à ce qu’on devine qui c’est."
+    }
+  },
+  {
+    id: 26,
+    category: "punishment",
+    text: {
+      en: "Speak with a French accent for the next 3 questions.",
+      it: "Parla con accento francese per le 3 prossime domande",
+      fr: "Parle avec un accent français pendant les 3 prochaines questions."
+    }
+  },
+  {
+    id: 27,
+    category: "dare",
+    text: {
+      en: "Act out your first date with the bride, without using words.",
+      it: "Mima il tuo primo appuntamento con la sposa, senza usare parole",
+      fr: "Mime ton premier rendez-vous avec la mariée, sans utiliser de mots."
+    }
+  },
+  {
+    id: 28,
+    category: "punishment",
+    text: {
+      en: "Add extra hot sauce on your next chip.",
+      it: "Metti una salsa extra sulla prossima chip",
+      fr: "Ajoute une dose de sauce piquante supplémentaire sur ta prochaine chips."
+    }
+  },
+  {
+    id: 29,
+    category: "punishment",
+    text: {
+      en: "Strut around the table like on a catwalk with background music (tapped on the table).",
+      it: "Sfila attorno al tavolo come in una passerella con una musica di sottofondo (battuta sul tavolo).",
+      fr: "Défile autour de la table comme sur un podium avec une musique de fond (tapée sur la table)."
+    }
   }
 ];
 
